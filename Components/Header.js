55f8df6ad4b8ff71598806580ee142ea10efea47
Michael McGarvey <71113885+MochaGamesDev@users.noreply.github.com>
2023-09-16 16:11:57 -0400

    Update the filters

--- Components/Header.js
+++ Components/Header.js
@@ -2,8 +2,9 @@ import { Alert, ScrollView, StyleSheet, TouchableOpacity, View, Text } from "rea
 import { SearchBar } from "@rneui/themed";
 import { useState } from "react";
 import FilterButton from "./FilterButton";
-import { faStar, faSchool, faTree, faDog, faMedkit, faBalanceScale, faHouseChimneyCrack } from "@fortawesome/free-solid-svg-icons";
+import { faStar, faHurricane, faDog, faMedkit, faBalanceScale, faHouseChimneyCrack } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { ToggleFavorite } from "../App";
 
 const height_proportion = '20%';
 
@@ -28,22 +29,6 @@ const styles = StyleSheet.create({
 
 export default function Header({setSearchResults, data}) {
     const [text, setText] = useState(null)
-
-    const onStarPress = () => {
-        Alert.alert('Favorites', 'Awaiting Alain\'s Favorites Page', 
-        [
-            {
-                text: 'Cancel',
-                onPress: () => {console.log('Canceled')},
-                style: 'cancel',
-            },
-            {
-                text: 'OK',
-                onPress: () => {console.log('OK')},
-            }
-        ]
-        );
-    }
 
     const handleSearch = (searchString) => {
         const newSearchResults = data.filter((charity) => {
@@ -83,7 +68,7 @@ export default function Header({setSearchResults, data}) {
                     placeholder="Search"
                     />
                 </View>
-                <TouchableOpacity onPress={ onStarPress } 
+                <TouchableOpacity onPress={() => ToggleFavorite()} 
                     style={{
                         height: 50,
                         width: 50,
@@ -103,12 +88,11 @@ export default function Header({setSearchResults, data}) {
                 </TouchableOpacity>
             </View>
             <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={{marginHorizontal:20, overflow: "hidden"}}>
-                <FilterButton title="Environmental" zoomId="1" imgInfo={faTree}/>
-                <FilterButton title="Education" zoomId="2" imgInfo={faSchool}/>
-                <FilterButton title="Animal Relief" zoomId="3" imgInfo={faDog}/>
-                <FilterButton title="Impoverished Areas" zoomId="4" imgInfo={faHouseChimneyCrack}/>
+                <FilterButton title="Disaster Relief" zoomId="1" imgInfo={faHurricane}/>
+                <FilterButton title="Impoverished Areas" zoomId="3" imgInfo={faHouseChimneyCrack}/>
                 <FilterButton title="Social Justice" zoomId="5" imgInfo={faBalanceScale}/>
-                <FilterButton title="Medical" zoomId="6" imgInfo={faMedkit}/>
+                <FilterButton title="Animal Relief" zoomId="2" imgInfo={faDog}/>
+                <FilterButton title="Medical" zoomId="4" imgInfo={faMedkit}/>
             </ScrollView>
         </View>
     );
