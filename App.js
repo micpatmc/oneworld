21bdd20d5b23ddbf93e79ae34e265cc693fe2ffe
christiandur4nt <christian.durant88@gmail.com>
2023-09-16 07:40:02 -0400

    Fix images in search results

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View} from 'react-native';
 import { useState } from 'react';
 import Map from './Components/Map';
 import Header from './Components/Header';
@@ -21,11 +21,16 @@ export default function App() {
           {
             searchResults.map((value, index) => {
               return (
-                <Card wrapperStyle={styles.searchResult}>
+                <Card key={index}>
+                  <Card.Image style={styles.cardImage} source={{uri: value.Image}}/>
                   <Card.Title>{value.Name}</Card.Title>
+                  <Text style={{textAlign:'center', marginBottom: 20}}>{value.Tagline}</Text>
                   <Card.Divider />
-                  <View>
-                    
+                  <View style={styles.searchResult}>
+                    <Text style={styles.cardInfo}><Text style={{fontWeight: "bold"}}>Type:</Text> {value.Type}</Text>
+                    <Text style={styles.cardInfo}><Text style={{fontWeight: "bold"}}>Address:</Text> {value.Address}</Text>
+                    <Text style={styles.cardInfo}><Text style={{fontWeight: "bold"}}>Website:</Text> {value.Website}</Text>
+                    <Text style={styles.cardInfo}><Text style={{fontWeight: "bold"}}>Location:</Text> {value.Location}</Text>
                   </View>
                 </Card>
               );
@@ -46,7 +51,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   searchResult: {
-    height: 200,
+    height: 150,
+  },
+  cardImage: {
+    marginBottom: 10,
+    borderRadius: 5,
+  },
+  cardInfo: {
+    marginVertical: 10,
   },
   debug: {
     borderWidth: 5,
