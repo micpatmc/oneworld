c222a5192274d876d1ee53f945e46cb5c6ae3457
christiandur4nt <christian.durant88@gmail.com>
2023-09-16 10:56:10 -0400

    Edit search result cards and fix keyboard dismiss

--- App.js
+++ App.js
@@ -1,46 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { ScrollView, StyleSheet, Text, View} from 'react-native';
+import { Keyboard, ScrollView, StyleSheet, Text, TouchableWithoutFeedback, View} from 'react-native';
 import { useState } from 'react';
 import Map from './Components/Map';
 import Header from './Components/Header';
-import { Card } from '@rneui/themed';
+import { Button, Card } from '@rneui/themed';
 import data from "./data.json"
 
 export default function App() {
   const [searchResults, setSearchResults] = useState([])
 
   return (
-    <View style={styles.container}>
-      <Header setSearchResults={setSearchResults} data={data}/>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <Header setSearchResults={setSearchResults} data={data}/>
 
-      { /* Determine whether to render map or search results */ }
-      {searchResults.length == 0
-        ? <Map />
-        : 
-        <ScrollView style={{width:'100%'}}>
-          {
-            searchResults.map((value, index) => {
-              return (
-                <Card key={index}>
-                  <Card.Image style={styles.cardImage} source={{uri: value.Image}}/>
-                  <Card.Title>{value.Name}</Card.Title>
-                  <Text style={{textAlign:'center', marginBottom: 20}}>{value.Tagline}</Text>
-                  <Card.Divider />
-                  <View style={styles.searchResult}>
-                    <Text style={styles.cardInfo}><Text style={{fontWeight: "bold"}}>Type:</Text> {value.Type}</Text>
-                    <Text style={styles.cardInfo}><Text style={{fontWeight: "bold"}}>Address:</Text> {value.Address}</Text>
-                    <Text style={styles.cardInfo}><Text style={{fontWeight: "bold"}}>Website:</Text> {value.Website}</Text>
-                    <Text style={styles.cardInfo}><Text style={{fontWeight: "bold"}}>Location:</Text> {value.Location}</Text>
-                  </View>
-                </Card>
-              );
-            })
-          }
-        </ScrollView>
-      }
+        { /* Determine whether to render map or search results */ }
+        {searchResults.length == 0 ?
+          <Map /> :
+          <ScrollView showsVerticalScrollIndicator={false}>
+            {
+              searchResults.map((value, index) => {
+                return (
+                  <Card key={index}>
+                    <Card.Image style={styles.cardImage} source={{uri: value.Image}}/>
+                    <Card.Title>{value.Name}</Card.Title>
+                    <Text style={{textAlign:'center', marginBottom: 20}}>{value.Tagline}</Text>
+                    <Card.Divider />
+                    <View style={styles.searchResult}>
+                      <Text style={styles.cardInfo}><Text style={{fontWeight: "bold"}}>Type:</Text> {value.Type}</Text>
+                      <Text style={styles.cardInfo}><Text style={{fontWeight: "bold"}}>Address:</Text> {value.Address}</Text>
+                      <Text style={styles.cardInfo}><Text style={{fontWeight: "bold"}}>Website:</Text> {value.Website}</Text>
+                      <Text style={styles.cardInfo}><Text style={{fontWeight: "bold"}}>Location:</Text> {value.Location}</Text>
+                      <Button style={styles.cardButton} size='lg' buttonStyle={{backgroundColor:"black"}}>
+                        <Text style={{fontWeight:'bold', color:"white"}}>Go To Location</Text>
+                      </Button>
+                    </View>
+                  </Card>
+                );
+              })
+            }
+          </ScrollView>
+        }
 
-      <StatusBar style="auto" />
-    </View>
+        <StatusBar style="auto" />
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -50,15 +54,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  searchResult: {
-    height: 150,
-  },
   cardImage: {
     marginBottom: 10,
     borderRadius: 5,
   },
   cardInfo: {
     marginVertical: 10,
+  },
+  cardButton: {
+    marginLeft: 'auto',
   },
   debug: {
     borderWidth: 5,
