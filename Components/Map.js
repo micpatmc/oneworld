a780bbbacd4ea7f3693c3e4c76ed439fa3a92fcb
Michael McGarvey <71113885+MochaGamesDev@users.noreply.github.com>
2023-09-16 01:24:57 -0400

    Added additional marker(s)

--- Components/Map.js
+++ Components/Map.js
@@ -27,8 +27,33 @@ export default function Map() {
           <Callout tooltip>
             <View>
               <View style={styles.calloutBubble}>
-                <Text style={styles.calloutText}>dawdawdwd</Text>
-                <Text style={styles.calloutText}>Something else</Text>
+                <Text style={styles.calloutText}>Name</Text>
+                <Text style={styles.calloutText}>Address</Text>
+                {/* <Image  */}
+                  {/* style={styles.calloutImage} */}
+                  {/* source={require('../assets/pin.png')} */}
+                {/* /> */}
+              </View>
+              <View style={styles.arrowBorder}/>
+              <View style={styles.arrow}/>
+            </View>
+          </Callout>
+        </Marker>
+        <Marker
+          coordinate={{
+            latitude: 37,
+            longitude: -100,
+          }}
+          image={require('../assets/pin.png')}
+          title="Charity"
+          description="Wildfires"
+        >
+          <Callout tooltip>
+            <View>
+              <View style={styles.calloutBubble}>
+                <Text style={styles.calloutText}>Charity2</Text>
+                <Text style={styles.calloutText}>Address</Text>
+                <Text style={styles.calloutText}></Text>
                 {/* <Image  */}
                   {/* style={styles.calloutImage} */}
                   {/* source={require('../assets/pin.png')} */}
@@ -60,17 +85,31 @@ const styles = StyleSheet.create({
     alignSelf: 'flex-start',
     backgroundColor: '#fff',
     borderRadius: 6,
+    borderColor: '#ccc',
+    borderWidth: 0.5,
+    padding: 15,
+    width: 150
   },
   calloutText: {
     textAlign: 'left',
     fontSize: 16,
     marginBottom: 5
   },
-  arrowBorder: {
-
-  },
   arrow: {
-
+    backgroundColor: 'transparent',
+    borderColor: 'transparent',
+    borderTopColor: '#fff',
+    borderWidth: 16,
+    alignSelf: 'center',
+    marginTop: -32
+  },
+  arrowBorder: {
+    backgroundColor: 'transparent',
+    borderColor: 'transparent',
+    borderTopColor: '#007a87',
+    borderWidth: 16,
+    alignSelf: 'center',
+    marginTop: -0.5
   },
   calloutImage: {
 
